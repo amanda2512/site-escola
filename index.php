2ef81021aea01f9507768711e2cdd92ac68a74a3
amanda2512 <126569036+amanda2512@users.noreply.github.com>
2023-04-04 08:13:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href ="css/style.css" type="text/css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    
+	<div class="container">
+	    <div class="row">
+		 <nav class="col-md-2"
+	   <ul class-"list-unstyled">
+      <li><a href"#">Home</a></li>
+      <li><a href"q1.html">Questao 1</a></li>
+      <li><a href"q1.html">Questao 2</a></li>
+      <li><a href"q1.html">Questao 3</a></li>
+      <li><a href"q1.html">Questao 4</a></li>
+
+	    <div class="col-4 col-md-2">col 1</div>
+		  <div class="col-4 col-md-4">col 2</div>
+		  <div class="col-4 col-md-4">col 3</div>
+		 
+  </ul>
+   <main class="col-md-9"></main>
+   <h1>Home</h1>
+   <p>
+  
+        Mussun Ipsum, cacilds vidis litro abertis. Copo furadis e disculpa é de bebadis, arcu quam euismod manga.sapien in monti palavirs quin num significa nadis i pareci la 
+
+   </p>
+</main>
+</div>
+</section>
+<footer class="text-white">
+</section class="container">
+  <dic class="row">
+  <ul class="col-sm-6 list-unstyled">
+    <li></h6>Links</h6></li>
+    <li>Home</li>
+    <li>cardapio</li>
+    <li>contato</li>
+   
+  </ul>
+ 
+
+      <ul class="col-sm-6">
+        <li><h6>Pizzas</h6></li>
+         <li>Tradicionais</li>
+         <li>Nobres</li>
+         <li>Doces</li>
+
+
+
+
+</ul>
+</div>
+</section>
+
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
